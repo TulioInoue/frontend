ee5ebfe3f74008c9d644f1c8ee9d549fb7eb4887
Túlio Inoue <140193266+TulioInoue@users.noreply.github.com>
2025-03-19 16:55:29 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Definindo a base para produção
-  build: {
-    outDir: 'dist', // Diretório de saída
-  },
 })
